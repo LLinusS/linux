7bf710527f060fcecdeb67b199d128b5662c9245
LLinusS <89519139+LLinusS@users.noreply.github.com>
2021-08-26 22:44:38 +0200

    worker2

--- sst.sh
+++ sst.sh
@@ -2,7 +2,7 @@
 sudo cat >> /etc/network/interfaces << EOF
 auto enp0s8
 iface enp0s8 inet static
-    address 192.168.1.101
+    address 192.168.1.102
     netmask 255.255.255.0
     network 192.168.1.0
     broadcast 192.168.1.255
